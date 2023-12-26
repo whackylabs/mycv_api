735d92b363dff23e621b414d92bda8f3b185cf21
Sidharth Juyal <213683+chunkyguy@users.noreply.github.com>
2023-12-26 13:14:03 +0100

    move client app to public

--- routes/root_path.js
+++ routes/root_path.js
@@ -1,9 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
+const path_1 = __importDefault(require("path"));
 const express_1 = require("express");
 exports.router = (0, express_1.Router)();
 exports.router.get("/", (req, res) => {
-    res.sendStatus(200);
+    res.sendFile(path_1.default.join(__dirname, "/public/index.html"));
 });
 //# sourceMappingURL=root_path.js.map
